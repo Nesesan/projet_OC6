c34b98d28be9b100c4693926c2b7bc19fa65c17d
Nesesan <lounes.benz@outlook.fr>
2025-07-02 20:20:39 +0200

    feat: Implementation of the several services with methods

--- back/src/main/java/com/openclassrooms/mddapi/service/impl/PostService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/impl/PostService.java
@@ -1,23 +1,57 @@
 package com.openclassrooms.mddapi.service.impl;
 
 import com.openclassrooms.mddapi.model.Post;
+import com.openclassrooms.mddapi.model.Topic;
+import com.openclassrooms.mddapi.model.User;
+import com.openclassrooms.mddapi.repository.PostRepository;
+import com.openclassrooms.mddapi.repository.TopicRepository;
+import com.openclassrooms.mddapi.repository.UserRepository;
 import com.openclassrooms.mddapi.service.IPostService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
 public class PostService  implements IPostService {
+
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final TopicRepository topicRepository;
+
+    public PostService(PostRepository postRepository, UserRepository userRepository, TopicRepository topicRepository) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+        this.topicRepository = topicRepository;
+    }
+
+
     @Override
     public Post createPost(Post post) {
-        return null;
+        Post newPost = new Post();
+        newPost.setTitle(post.getTitle());
+        newPost.setContent(post.getContent());
+        newPost.setCreatedAt(LocalDateTime.now());
+
+        User author =   userRepository.findByEmail(post.getAuthor().getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        newPost.setAuthor(author);
+
+        Topic topic = topicRepository.findById(post.getTopic().getId())
+                .orElseThrow(() -> new UsernameNotFoundException("Topic not found"));
+        newPost.setTopic(topic);
+
+        return postRepository.save(newPost);
     }
 
     @Override
     public List<Post> getAllPosts() {
-        return List.of();
+        return postRepository.findAll();
     }
 
     @Override
     public Post getPostById(Long id) {
-        return null;
+        return postRepository.findById(id).orElse(null);
     }
 }
