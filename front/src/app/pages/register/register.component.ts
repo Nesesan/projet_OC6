c464e12ef6889729ce93fca0ed67d2229fd8bbff
Nesesan <lounes.benz@outlook.fr>
2025-07-06 23:24:33 +0200

    Feat: - update CSS, HTML and TypeScript functionalities for register

--- front/src/app/pages/register/register.component.ts
+++ front/src/app/pages/register/register.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Router} from "@angular/router";
+import {AuthService} from "../../services/authService";
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  registerForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthService) {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe({
+          next: () => {
+            alert("Register successfully");
+            this.router.navigate(['/login']);
+          },
+          error: (err: { message: string; }) => {
+            alert('An error has occurred: ' + err.message);
+          }
+        }
+      )
+    }
+  }
+
+  back() {
+    this.router.navigate(['/'])
+  }
 }
